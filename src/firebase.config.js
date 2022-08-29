b2f7772e94310dc640c077625f506477343b5207
Suleman Saddique <sulemansddq@gmail.com>
2022-08-29 09:53:54 +0500

    initial commit

---
+++ src/firebase.config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
+
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+   apiKey: "AIzaSyDpPgx9FFrfg4yPxS27gTj_3O8YTpNJnvA",
+   authDomain: "property-market-decb6.firebaseapp.com",
+   projectId: "property-market-decb6",
+   storageBucket: "property-market-decb6.appspot.com",
+   messagingSenderId: "1069867689989",
+   appId: "1:1069867689989:web:c6c6887bbfe414868c946c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
+import visibilityIcon from "../assets/svg/visibilityIcon.svg";
+
+function SignUp() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const { name, email, password } = formData;
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+  return (
+    <>
+      <div className="pageContainer">
+        <header>
+          <p className="pageHeader">Sign Up</p>
+        </header>
+        <form>
+          <input
+            type="text"
+            className="nameInput"
+            id="name"
+            placeholder="Name"
+            value={name}
+            onChange={onChange}
+          />
+          <input
+            type="email"
+            className="emailInput"
+            id="email"
+            placeholder="Email"
+            value={email}
+            onChange={onChange}
+          />
+          <div className="passwordInputDiv">
+            <input
+              className="passwordInput"
+              type={showPassword ? "text" : "password"}
+              id="password"
+              placeholder="Password"
+              value={password}
+              onChange={onChange}
+            />
+            <img
+              src={visibilityIcon}
+              alt="show Password"
+              className="showPassword"
+              onClick={() => setShowPassword((prevState) => !prevState)}
+            />
+          </div>
+          <Link to="/forgot-password" className="forgotPasswordLink">
+            Forgot Password ?
+          </Link>
+
+          <div className="signInBar">
+            <p className="signInText">Register</p>
+            <button className="signInButton">
+              <ArrowRightIcon fill="#ffffff" width="34px" height="34px" />
+            </button>
+          </div>
+        </form>
+
+        {/* Google OAuth */}
+        <Link to="/sign-in" className="registerLink">
+          Already have an account? Sign In
+        </Link>
+      </div>
+    </>
+  );
+}
+
+export default SignUp;
